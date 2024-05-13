4671e550937eca21a073e7de91e57abcab952a37
Miguel Ibarra <miguelibsan@gmail.com>
2024-05-13 12:07:03 -0600

    python carpeta

---
+++ Python/Prueba.py
@@ -0,0 +1,8 @@
+import pyautogui
+
+# Define la posición a la que quieres mover el ratón
+x = 500  # posición horizontal en píxeles
+y = 300  # posición vertical en píxeles
+
+# Mueve el ratón a la posición especificada
+pyautogui.moveTo(x, y)
